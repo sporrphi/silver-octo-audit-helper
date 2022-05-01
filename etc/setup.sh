a39bf6884eba5c56b8bcb89758cfca7c21f62bcd
Philipp Sporrer <philipp.sporrer@outlook.com>
2022-05-01 19:59:10 +0000

    Added better setup

--- etc/setup.sh
+++ etc/setup.sh
@@ -4,12 +4,16 @@
 
 set -e;
 
+export PROJECT_ROOT = ../.
+
 echo "Start setup.sh"
 
 # Setting up git config
 git config --global user.name "Philipp Sporrer"
 git config --global user.email "59526058+sporrphi@users.noreply.github.com"
 
+# make file in bin executable
+chmod a+x PROJECT_ROOT/bin/npm-audit-helper
 
 echo "Setup finished."
 
